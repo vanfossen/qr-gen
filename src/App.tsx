1fdaef60a8aaa6a60181bc006c5a782aa1ee60ba
vanfossen <35882058+vanfossen@users.noreply.github.com>
2025-03-09 23:15:38 -0400

    simple generator working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import React, { useState } from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { QRCodeSVG } from "qrcode.react";
+
+const TEXT_AREA_LENGTH = 512;
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const handleValueChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const value = event.target.value;
+    setValue(value);
+  };
+
   return (
     <>
       <Navbar />
@@ -10,10 +21,23 @@ function App() {
         <fieldset className="fieldset">
           <legend className="fieldset-legend">QR Code Value</legend>
           <textarea
+            id="qr-value"
             className="textarea h-64 w-128"
             placeholder="Type here"
+            maxLength={TEXT_AREA_LENGTH}
+            value={value}
+            onChange={handleValueChange}
           ></textarea>
+          <div className="fieldset-label">
+            Characters left: {TEXT_AREA_LENGTH - value.length}
+          </div>
         </fieldset>
+
+        {value && (
+          <div className="m-4">
+            <QRCodeSVG value={value} marginSize={1} size={384} />
+          </div>
+        )}
       </div>
 
       <Footer />
