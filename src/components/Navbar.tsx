6bf763458363746e0a3d0e1972de9d8ed3dfe141
vanfossen <35882058+vanfossen@users.noreply.github.com>
2025-03-15 17:31:57 -0400

    remove theme controller

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,8 @@
-import ThemeController from "./ThemeController";
-
 function Navbar() {
   return (
     <nav className="navbar bg-base-100 shadow-sm">
       <div className="navbar-start">
         <a className="px-4 text-xl font-semibold">qr-gen</a>
-      </div>
-      <div className="navbar-end px-4">
-        <ThemeController />
       </div>
     </nav>
   );
